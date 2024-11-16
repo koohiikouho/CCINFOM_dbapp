af9c17c95810dd30a016fbb7f1c6f42725196e4e
S16ABeaA <159003129+S16ABeaA@users.noreply.github.com>
2024-11-16 15:04:17 +0800

    Update Controller.java

--- Controller.java
+++ Controller.java
@@ -1,5 +1,8 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
 import javax.swing.JOptionPane;
 import javax.swing.event.DocumentEvent;
@@ -18,15 +21,25 @@ public class Controller implements ActionListener, DocumentListener{
 	
 	public void actionPerformed(ActionEvent e) {
 		String command = e.getActionCommand();
-		//JButton source = (JButton) e.getSource();
-		//System.out.println("Button pressed: " + source.getText());
+		PreparedStatement pstmt;
+		
+		try {
+		String url = "jdbc:mysql://147.185.221.23:51100/dbmovieRental";
+		String username = "user";
+		String password= "12345";
+//			String url = "jdbc:mysql://localhost:3306/dbmovieRental";
+//		    String username = "root";
+//		    String password = "dl_MySQL_su";
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection connections = DriverManager.getConnection(url,username,password);
 		
 		switch(command) {
 		
 		case "TableInput":
 			System.out.println("You clicked TableInput");
 			gui.createTableInputPanel();
-	
+			gui.refreshAdminTable();
+			ClearAdminInputs();
 			break;
 			
 		case "RecordManagement":
@@ -47,10 +60,64 @@ public class Controller implements ActionListener, DocumentListener{
 			System.out.println("return to main");
 			gui.createMainMenuPanel();
 			break;
+		
+		case "AddInAdminTable":
+			System.out.println("add clicked");
+			
+			//admin table
+			int admin_no1, admin_level1;
+			String first_name1, last_name1, admin_password1;
+			
+			String InsertAdmin = " insert into admins (admin_no, first_name, last_name, password, admin_level)"
+				    + " values (?, ?, ?, ?, ?)";
+			pstmt = connections.prepareStatement(InsertAdmin);
+			
+			admin_no1 = gui.getAdminNumber();
+			first_name1 = gui.getAdminFirstName();
+			last_name1 = gui.getAdminLastName();
+			admin_password1 = gui.getAdminPassword();
+			admin_level1 = gui.getAdminLevel();
+			
+			pstmt.setInt(1,admin_no1);
+			pstmt.setString(2, first_name1);
+			pstmt.setString(3,last_name1);
+			pstmt.setString(4, admin_password1);
+			pstmt.setInt(5, admin_level1);
+			pstmt.execute();
+			gui.refreshAdminTable();
+			ClearAdminInputs();
+			break;
+			
+		case "DeleteInAdminTable":
+			
+			admin_no1 = gui.getAdminNumber();
+			first_name1 = gui.getAdminFirstName();
+			last_name1 = gui.getAdminLastName();
+			admin_password1 = gui.getAdminPassword();
+			admin_level1 = gui.getAdminLevel();
+			
+			String DeleteAdmin = " DELETE FROM admins WHERE admin_no = " + admin_no1 ;
+			pstmt = connections.prepareStatement(DeleteAdmin);
+			pstmt.execute();
+			gui.refreshAdminTable();
+
+			break;
 			
 		}
+
+	}
+	catch (Exception ex) {
+		System.out.println(ex);
+	}
 	}
 	
+	public void ClearAdminInputs() {
+		gui.setAdminFirstName("");
+		gui.setAdminLastName("");
+		gui.setAdminNumber("");
+		gui.setAdminPassword("");
+		gui.setAdminLevel("");
+	}
 	
 	@Override
 	public void insertUpdate(DocumentEvent e) {
